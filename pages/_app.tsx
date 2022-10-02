67c026ae7684225fbf15bebaa914f6f4d9876e06
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-01 21:49:50 -0300

    Persist user authentication state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
-import { AppProps } from "next/app";
+import { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 import './global.css';
 
-const App = ({ Component, pageProps }: AppProps) => {
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   );
-};
-
-export default App;
+}
