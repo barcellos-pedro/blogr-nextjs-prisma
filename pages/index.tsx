088d6eb196d1e482d46d679304e7a44f546914a9
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-03 20:50:41 -0300

    Use service on staticProps and swr to refresh data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,67 @@
-import React from 'react';
 import { GetStaticProps } from 'next';
 import { PostModel } from '../utils/post-model';
 import { Layout } from '../components/Layout';
 import { Header } from '../components/Header';
-
 import { PostList } from '../components/PostList';
-import prisma from '../lib/prisma';
 import Head from 'next/head';
+import { postsService } from '../services/posts-service';
+import useSWR from 'swr';
+import { ArrowPathIcon, CloudArrowDownIcon } from '@heroicons/react/24/outline';
 
 type HomeProps = {
-  feed: PostModel[];
+  posts: PostModel[];
 };
 
-export default function Home({ feed }: HomeProps) {
+export default function Home({ posts }: HomeProps) {
+  const {
+    data: feed = posts,
+    error,
+    mutate,
+    isValidating,
+  } = useSWR('/api/data', postsService.getPosts);
+
+  const updatePosts = async () => {
+    // update feed data with swr function
+    await mutate();
+  };
+
   return (
     <Layout home>
       <Head>
         <title>Home</title>
       </Head>
+
       <Header />
-      <h2 className="text-black font-bold text-2xl mb-7">Public Feed</h2>
+
+      <div className="flex justify-between items-center">
+        <h2 className="text-black font-bold text-2xl mb-7">Public Feed</h2>
+        <button
+          onClick={updatePosts}
+          className="flex gap-2 bg-white p-2 rounded border border-transparent hover:border-blue-400 duration-300"
+        >
+          {isValidating ? (
+            <CloudArrowDownIcon
+              className="animate-pulse"
+              width={20}
+              height={20}
+            />
+          ) : (
+            <>
+              Refresh
+              <ArrowPathIcon width={20} height={20} />
+            </>
+          )}
+        </button>
+      </div>
+
       <div className="bg-white rounded p-8">
-        {!feed.length ? (
+        {error && (
+          <p className="text-center text-xl">
+            Error when fetching latest posts. Try again.
+          </p>
+        )}
+
+        {!feed?.length ? (
           <p>There are no posts yet</p>
         ) : (
           <PostList items={feed} />
@@ -32,19 +72,10 @@ export default function Home({ feed }: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const feed = await prisma.post.findMany({
-    where: { published: true },
-    include: {
-      author: {
-        select: {
-          name: true,
-        },
-      },
-    },
-  });
+  const posts = await postsService.getPosts();
 
   return {
-    props: { feed },
-    revalidate: 10000,
+    props: { posts },
+    revalidate: 300,
   };
 };
