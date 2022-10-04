5b0b0d9f6f661758706d7db74e7a897636280463
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-04 09:24:27 -0300

    Create login page

---
+++ pages/login.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import { Layout } from '../components/Layout';
+import { FaGithub, FaGoogle } from 'react-icons/fa';
+
+export default function LoginPage() {
+  return (
+    <Layout home className="flex justify-center items-center h-screen bg-gradient-to-t from-[#1d2a5e] to-[#40bda7]">
+      <Head>
+        <title>Blog - Login</title>
+      </Head>
+
+      <div className="bg-white p-5 rounded ring-8 ring-sky-200/40">
+        <h1 className="text-center text-2xl font-bold">Log in</h1>
+
+        <hr className="w-10/12 mx-auto my-4 border-zinc-300" />
+
+        <div className="flex flex-col items-center gap-5">
+          <Link href="/api/auth/signin">
+            <a className="bg-black flex justify-between gap-3 items-center font-semibold px-3 py-2 text-white rounded border hover:bg-white/90 hover:text-black hover:border-black duration-300 ">
+              <FaGithub />
+              Continue with Github
+            </a>
+          </Link>
+          <Link href="/api/auth/signin">
+            <a className="bg-red-500 flex justify-between gap-3 items-center font-semibold px-3 py-2 text-white rounded border hover:bg-white hover:text-red-500 hover:border-red-500 duration-300">
+              <FaGoogle />
+              Continue with Google
+            </a>
+          </Link>
+        </div>
+      </div>
+    </Layout>
+  );
+}
