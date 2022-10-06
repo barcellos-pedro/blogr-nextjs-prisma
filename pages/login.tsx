e62d7c62c459ae2ef642ae6ecafaf2d994092f26
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-06 10:56:43 -0300

    Refactor client side page protection

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,10 +4,10 @@ import { signIn, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 
 export default function LoginPage() {
-  const { data: session } = useSession();
+  const { status } = useSession();
   const router = useRouter();
 
-  if (session) {
+  if (status === 'authenticated') {
     router.push('/');
   }
 
