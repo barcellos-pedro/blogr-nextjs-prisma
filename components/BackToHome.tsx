7a14788b274cbcd6a097eb50ef65a7debe34e1cb
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-06 10:08:06 -0300

    Add hover style

--- components/BackToHome.tsx
+++ components/BackToHome.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export const BackToHome = () => {
   return (
     <Link href="/">
-      <a className={''}>&larr; Back to home</a>
+      <a className="hover:underline">&larr; Back to home</a>
     </Link>
   );
 };
