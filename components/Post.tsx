e32f385c66422380534a3a911804f721ef680d31
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-07 14:37:23 -0300

    Use PostMenu

--- components/Post.tsx
+++ components/Post.tsx
@@ -2,24 +2,36 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import Link from 'next/link';
 import { PostModel } from '../utils/post-model';
+import { useSession } from 'next-auth/react';
+import { PostMenu } from './PostMenu';
 
 type PostProps = {
   post: PostModel;
 };
 
 export const Post = ({ post }: PostProps) => {
-  const postTitle = post.published ? post.title : `${post.title} (Draft)`;
-  const authorName = post.author.name ? post.author.name : 'Unknown author';
+  const { data: session } = useSession();
+  const {
+    id,
+    title,
+    content,
+    author: { name },
+  } = post;
+  const authorName = name || 'Unknown author';
+  const isUserPost = () => session?.user?.name == post.author?.name;
 
   return (
     <>
-      <Link href={`/posts/${post.id}`}>
-        <a>
-          <h2 className="font-bold text-xl">{postTitle}</h2>
-        </a>
-      </Link>
+      <div className="flex justify-between">
+        <Link href={`/posts/${id}`}>
+          <a>
+            <h2 className="font-bold text-xl">{title}</h2>
+          </a>
+        </Link>
+        {!!isUserPost() && <PostMenu id={id} />}
+      </div>
       <small className="text-zinc-500">By {authorName}</small>
-      <ReactMarkdown children={post.content} />
+      <ReactMarkdown children={content} />
     </>
   );
 };
