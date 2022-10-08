72abb3957927e0847442006dd6921ba384bf1972
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-08 14:27:18 -0300

    Add try catch

--- pages/api/posts/[id].ts
+++ pages/api/posts/[id].ts
@@ -1,18 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = async (request: NextApiRequest, response: NextApiResponse) => {
-  const post = await prisma.post.findUniqueOrThrow({
-    where: {
-      id: String(request.query?.id),
-    },
-    include: {
-      author: {
-        select: { name: true },
+  try {
+    const post = await prisma.post.findUniqueOrThrow({
+      where: {
+        id: String(request.query?.id),
       },
-    },
-  });
+      include: {
+        author: {
+          select: { name: true, email: true },
+        },
+      },
+    });
 
-  return response.status(200).json(post);
+    return response.status(200).json(post);
+  } catch (error) {
+    return response.status(500).send('Error fething a post. Try again');
+  }
 };
 
 export default handler;
