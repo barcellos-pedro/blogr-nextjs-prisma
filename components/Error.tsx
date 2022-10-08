a39d2d9b22e9264e8bdd9c142aaefeec6ac868b9
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-08 14:26:52 -0300

    Change Unauthorized to Error component

---
+++ components/Error.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+
+interface ErrorProps {
+  title: string;
+  description: string;
+  buttonText: string;
+  navigateTo: string;
+}
+
+export const Error = ({
+  title,
+  description,
+  buttonText,
+  navigateTo = '/',
+}: ErrorProps) => {
+  return (
+    <main className="flex flex-col gap-3 justify-center items-center h-screen">
+      <h1 className="text-xl font-bold">{title}</h1>
+      <p className="text-zinc-500">{description}</p>
+      <Link href={navigateTo}>
+        <a className="bg-black text-white px-5 py-2 rounded">{buttonText}</a>
+      </Link>
+    </main>
+  );
+};
