a736aa5c122bbca80a9bb396276e62f03fae6ffb
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-08 14:04:49 -0300

    Add error handler for /

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -1,19 +1,22 @@
 import prisma from '../../../lib/prisma';
 
 const handler = async (request, response) => {
-  const posts = await prisma.post.findMany({
-    where: { published: true },
-    include: {
-      author: {
-        select: {
-          name: true,
-          email: true,
+  try {
+    const posts = await prisma.post.findMany({
+      where: { published: true },
+      include: {
+        author: {
+          select: {
+            name: true,
+            email: true,
+          },
         },
       },
-    },
-  });
-
-  return response.status(200).json(posts);
+    });
+    return response.status(200).json(posts);
+  } catch (error) {
+    return response.status(500).send('Error fething posts. Try again');
+  }
 };
 
 export default handler;
