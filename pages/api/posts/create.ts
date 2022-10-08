7530da11456d6f1a676de54e3a357442d5fb09a0
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-08 16:09:55 -0300

    Add try/catch for /create

--- pages/api/posts/create.ts
+++ pages/api/posts/create.ts
@@ -3,24 +3,28 @@ import { getSession } from 'next-auth/react';
 import prisma from '../../../lib/prisma';
 
 const handler = async (request: NextApiRequest, response: NextApiResponse) => {
-  const { title, content, publish } = request.body;
-  const published = publish === 'on' ? true : false;
-  const session = await getSession({ req: request });
+  try {
+    const { title, content, publish } = request.body;
+    const published = publish === 'on' ? true : false;
+    const session = await getSession({ req: request });
 
-  const newPost = await prisma.post.create({
-    data: {
-      title,
-      content,
-      published,
-      author: {
-        connect: {
-          email: session?.user?.email,
+    const newPost = await prisma.post.create({
+      data: {
+        title,
+        content,
+        published,
+        author: {
+          connect: {
+            email: session?.user?.email,
+          },
         },
       },
-    },
-  });
+    });
 
-  return response.status(201).json(newPost);
+    return response.status(201).json(newPost);
+  } catch (error) {
+    return response.status(500).send('Error creating a post. Try again');
+  }
 };
 
 export default handler;
