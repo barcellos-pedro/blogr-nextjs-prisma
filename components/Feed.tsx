04adc9b0c8176342b246034b2e796d4796d61e6f
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-08 14:02:12 -0300

    Create Feed component

---
+++ components/Feed.tsx
@@ -0,0 +1,20 @@
+import { PostModel } from '../types/post-model';
+import { PostList } from './PostList';
+
+interface FeedProps {
+  error?: string;
+  data: PostModel[];
+}
+
+export const Feed = ({ error, data }: FeedProps) => {
+  if (error && !data.length) {
+    return (
+      <>
+        <p className="text-zinc-500 mb-3">{error}</p>
+        <p>There are no posts yet</p>
+      </>
+    );
+  }
+
+  return <PostList data={data} />;
+};
