6fe9f182ee35a319398d72c6633bce2d3320ab97
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-08 16:17:00 -0300

    Add prop

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -3,9 +3,22 @@ import { ArrowPathIcon } from '@heroicons/react/24/solid';
 interface SpinnerProps {
   width: number;
   height: number;
+  fullscreen?: boolean;
 }
 
-export const Spinner = ({ width = 25, height = 25 }: SpinnerProps) => {
+export const Spinner = ({
+  width = 25,
+  height = 25,
+  fullscreen = false,
+}: SpinnerProps) => {
+  if (fullscreen) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <ArrowPathIcon width={width} height={height} className="animate-spin" />
+      </div>
+    );
+  }
+
   return (
     <ArrowPathIcon width={width} height={height} className="animate-spin" />
   );
