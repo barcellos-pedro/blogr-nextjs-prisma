9fd6a443414e9043747fac568b24e93d9d20c1db
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-08 11:05:55 -0300

    Add props

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,5 +1,12 @@
 import { ArrowPathIcon } from '@heroicons/react/24/solid';
 
-export const Spinner = () => {
-  return <ArrowPathIcon width={25} height={25} className="animate-spin" />;
+interface SpinnerProps {
+  width: number;
+  height: number;
+}
+
+export const Spinner = ({ width = 25, height = 25 }: SpinnerProps) => {
+  return (
+    <ArrowPathIcon width={width} height={height} className="animate-spin" />
+  );
 };
