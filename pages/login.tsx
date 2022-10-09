50b6435b55e4f416ccc3f1e65fd69919c22219d0
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-09 15:28:36 -0300

    Refactor login

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import { FaGithub, FaGoogle } from 'react-icons/fa';
 import { signIn, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import { Spinner } from '../components/Spinner';
 
 export default function LoginPage() {
   const { status } = useSession();
@@ -18,26 +19,32 @@ export default function LoginPage() {
       </Head>
 
       <div className="bg-white p-5 rounded ring-8 ring-sky-200/40">
-        <h1 className="text-center text-2xl font-bold">Log in</h1>
+        {status === 'loading' && <Spinner width={30} height={30} />}
 
-        <hr className="w-10/12 mx-auto my-4 border-zinc-300" />
+        {status == 'unauthenticated' && (
+          <>
+            <h1 className="text-center text-2xl font-bold">Log in</h1>
 
-        <div className="flex flex-col items-center gap-5">
-          <button
-            onClick={() => signIn('github', { callbackUrl: '/' })}
-            className="bg-black flex justify-between gap-3 items-center font-semibold px-3 py-2 text-white rounded border hover:bg-white/90 hover:text-black hover:border-black duration-300 "
-          >
-            <FaGithub />
-            Continue with Github
-          </button>
-          <button
-            onClick={() => signIn('google', { callbackUrl: '/' })}
-            className="bg-red-500 flex justify-between gap-3 items-center font-semibold px-3 py-2 text-white rounded border hover:bg-white hover:text-red-500 hover:border-red-500 duration-300"
-          >
-            <FaGoogle />
-            Continue with Google
-          </button>
-        </div>
+            <hr className="w-10/12 mx-auto my-4 border-zinc-300" />
+
+            <div className="flex flex-col items-center gap-5">
+              <button
+                onClick={() => signIn('github', { callbackUrl: '/' })}
+                className="bg-black flex justify-between gap-3 items-center font-semibold px-3 py-2 text-white rounded border hover:bg-white/90 hover:text-black hover:border-black duration-300 "
+              >
+                <FaGithub />
+                Continue with Github
+              </button>
+              <button
+                onClick={() => signIn('google', { callbackUrl: '/' })}
+                className="bg-red-500 flex justify-between gap-3 items-center font-semibold px-3 py-2 text-white rounded border hover:bg-white hover:text-red-500 hover:border-red-500 duration-300"
+              >
+                <FaGoogle />
+                Continue with Google
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </main>
   );
