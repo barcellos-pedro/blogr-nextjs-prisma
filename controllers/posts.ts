2becdc1a217cb057576cf05b36439b400c3a7bb0
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-09 15:53:14 -0300

    Create controller

---
+++ controllers/posts.ts
@@ -0,0 +1,54 @@
+import { NextApiRequest } from 'next';
+
+export const postsController = async (request: NextApiRequest) => {
+  try {
+    switch (request.method) {
+      case 'GET':
+        return await getById(request);
+      case 'PUT':
+        return await updateById(request);
+      case 'DELETE':
+        return await deleteById(request);
+      default:
+        throw new Error('Method not allowed');
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Methods/actions
+export const getById = async (request: NextApiRequest) => {
+  try {
+    const post = await prisma.post.findUniqueOrThrow({
+      where: {
+        id: String(request.query?.id),
+      },
+      include: {
+        author: {
+          select: { name: true, email: true },
+        },
+      },
+    });
+
+    return post;
+  } catch (error) {
+    throw new Error('Error fething a post. Try again');
+  }
+};
+
+export const updateById = async (request: NextApiRequest) => {
+  try {
+    // TODO: Implement
+  } catch (error) {
+    throw new Error('Error updatind a post. Try again');
+  }
+};
+
+export const deleteById = async (request: NextApiRequest) => {
+  try {
+    // TODO: Implement
+  } catch (error) {
+    throw new Error('Error deleteing a post. Try again');
+  }
+};
