c9bba0a42a977c266d54fba3a86c83b75f47d03e
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-09 15:50:12 -0300

    Create drafts api route

---
+++ pages/api/posts/drafts.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
+import prisma from '../../../lib/prisma';
+
+const handler = async (request: NextApiRequest, response: NextApiResponse) => {
+  try {
+    const session = await getSession({ req: request });
+
+    const drafts = await prisma.post.findMany({
+      where: {
+        published: false,
+        AND: {
+          author: { email: { equals: session?.user?.email } },
+        },
+      },
+      include: {
+        author: {
+          select: { name: true, email: true },
+        },
+      },
+    });
+
+    return response.status(200).json(drafts);
+  } catch (error) {
+    return response.status(500).send('Error fetching drafts. Try again.');
+  }
+};
+export default handler;
