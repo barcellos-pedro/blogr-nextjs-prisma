3e307c40b9e160388a1c885debce127d50fa9047
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-09 20:45:57 -0300

    Use form component

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,10 +1,5 @@
 import Head from 'next/head';
 import { Layout } from '../components/Layout';
-import {
-  CloudArrowDownIcon,
-  ExclamationCircleIcon,
-  HandThumbUpIcon,
-} from '@heroicons/react/24/outline';
 import { useSession } from 'next-auth/react';
 import { Error } from '../components/Error';
 import { FormEvent, useState } from 'react';
@@ -13,18 +8,19 @@ import { showToast } from '../utils/show-toast';
 import { useRouter } from 'next/router';
 import { getFormData } from '../utils/form-data';
 import { CreationStatus } from '../types/creation-status';
-import { ToggleButton } from '../components/ToggleButton';
 import { Spinner } from '../components/Spinner';
+import { Form } from '../components/form';
 
 export default function CreatePage() {
   const { NOT_STARTED, CREATING, SUCCESS, ERROR } = CreationStatus;
   const { data: session, status } = useSession();
   const router = useRouter();
 
-  const [publish, setPublish] = useState(false);
+  // Form fields and
+  const [isPublished, setIsPublished] = useState(false);
 
-  const [creationStatus, setCreationStatus] =
-    useState<CreationStatus>(NOT_STARTED);
+  // Status for visual feedback (icons)
+  const [creationStatus, setCreationStatus] = useState(NOT_STARTED);
 
   const createEvent = async (event: FormEvent) => {
     setCreationStatus(CREATING);
@@ -69,70 +65,12 @@ export default function CreatePage() {
 
       <h1 className="text-2xl font-bold">New Post</h1>
 
-      <form className="mt-5 mb-12 flex flex-col gap-5" onSubmit={createEvent}>
-        <div className="flex flex-col gap-1">
-          <label htmlFor="title" className="font-semibold">
-            Title
-          </label>
-          <input type="text" id="title" name="title" className="p-2 rounded" />
-        </div>
-
-        <div className="flex flex-col gap-1">
-          <label htmlFor="content" className="font-semibold">
-            Content
-          </label>
-          <textarea
-            id="content"
-            name="content"
-            cols={30}
-            rows={10}
-            className="p-3 rounded"
-          ></textarea>
-        </div>
-
-        <div className="flex items-center gap-2 mb-5">
-          <ToggleButton
-            id="publish"
-            label="Publish ?"
-            checked={publish}
-            onChange={() => setPublish(!publish)}
-          />
-        </div>
-
-        <div className="flex items-center justify-between">
-          <button
-            type="submit"
-            className="bg-orange-500 text-white rounded px-8 h-12 hover:bg-orange-600"
-          >
-            Create
-          </button>
-
-          {creationStatus === CREATING && (
-            <p className="flex gap-2">
-              <CloudArrowDownIcon
-                className="animate-pulse"
-                width={25}
-                height={25}
-              />
-              Saving...
-            </p>
-          )}
-
-          {creationStatus === SUCCESS && (
-            <p className="flex gap-2">
-              <HandThumbUpIcon width={25} height={25} />
-              Post created!
-            </p>
-          )}
-
-          {creationStatus === ERROR && (
-            <p className="flex gap-2">
-              <ExclamationCircleIcon width={25} height={25} />
-              Error creating the post. Try again.
-            </p>
-          )}
-        </div>
-      </form>
+      <Form
+        onSubmit={createEvent}
+        isPublished={isPublished}
+        onPublishedChange={() => setIsPublished(!isPublished)}
+        submittingStatus={creationStatus}
+      />
     </Layout>
   );
 }
