43ba3a2d57e21a1aa7de611fcc28d530b9607a2a
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-10 08:24:53 -0300

    Remove states

--- hooks/use-modal.ts
+++ hooks/use-modal.ts
@@ -1,10 +1,8 @@
 import { useState } from 'react';
 
-export const useModal = (intialTitle: string, intialText: string) => {
+export const useModal = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isPending, setIsPending] = useState(false);
-  const [title, setTitle] = useState(intialTitle);
-  const [description, setDescription] = useState(intialText);
   const closeModal = () => setIsOpen(false);
   const openModal = () => setIsOpen(true);
 
@@ -12,14 +10,10 @@ export const useModal = (intialTitle: string, intialText: string) => {
     values: {
       isOpen,
       isPending,
-      title,
-      description,
     },
     actions: {
       setIsOpen,
       setIsPending,
-      setTitle,
-      setDescription,
       closeModal,
       openModal,
     },
