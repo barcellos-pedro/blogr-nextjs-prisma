685f51eea3f1b6dc2fac02461f728991f545514c
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-09 21:53:20 -0300

    Modify prop

--- components/Error.tsx
+++ components/Error.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 interface ErrorProps {
   title: string;
-  description: string;
+  description?: string;
   buttonText: string;
   navigateTo: string;
 }
@@ -16,7 +16,7 @@ export const Error = ({
   return (
     <main className="flex flex-col gap-3 justify-center items-center h-screen">
       <h1 className="text-xl font-bold">{title}</h1>
-      <p className="text-zinc-500">{description}</p>
+      {description && <p className="text-zinc-500">{description}</p>}
       <Link href={navigateTo}>
         <a className="bg-black text-white px-5 py-2 rounded">{buttonText}</a>
       </Link>
