72b8eadae43f8600130e331d3931a4122b3750c7
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-20 22:55:48 -0300

    Fix session type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { ToastContainer } from 'react-toastify';
+import { Session } from 'next-auth';
 import 'react-toastify/dist/ReactToastify.css';
 import '../public/global.css';
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}: AppProps) {
+}: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
