174ee1ac4e6a3695e67eafafa28f456c4ea54e34
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-25 23:27:58 -0300

    Refactor interface inner types

--- types/user-posts-model.ts
+++ types/user-posts-model.ts
@@ -1,15 +1,7 @@
+import { Author } from './author';
+import { PostModel } from './post-model';
+
 export interface UserPostsModel {
-  id: string;
-  title: string;
-  content: string;
-  published: boolean;
-  authorId: string;
-  author: {
-    name: string;
-    email: string;
-    image: string;
-    _count: {
-      posts: number;
-    };
-  };
+  posts: PostModel[];
+  author: Author;
 }
