9b5618d8999760aa8328bcd4cd5ffbe4b3de3631
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-25 23:25:31 -0300

    Refactor route

--- pages/api/posts/users/[username].ts
+++ pages/api/posts/users/[username].ts
@@ -1,35 +1,49 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../../lib/prisma';
 
+const getPosts = (username: string) => {
+  return prisma.post.findMany({
+    where: {
+      published: true,
+      author: {
+        email: {
+          contains: username,
+        },
+      },
+    },
+    include: {
+      author: true,
+    },
+  });
+};
+
+const getAuthor = (username: string) => {
+  return prisma.user.findFirstOrThrow({
+    where: {
+      email: {
+        contains: username,
+      },
+    },
+    include: {
+      _count: {
+        select: {
+          posts: true,
+        },
+      },
+    },
+  });
+};
+
 const handler = async (request: NextApiRequest, response: NextApiResponse) => {
   try {
     const username = request.query?.username as string;
-    const posts = await prisma.post.findMany({
-      where: {
-        published: true,
-        author: {
-          email: {
-            contains: username,
-          },
-        },
-      },
-      include: {
-        author: {
-          select: {
-            name: true,
-            email: true,
-            image: true,
-            _count: {
-              select: {
-                posts: true,
-              },
-            },
-          },
-        },
-      },
-    });
 
-    return response.status(200).json(posts);
+    const [posts, author] = await Promise.all([
+      getPosts(username),
+      getAuthor(username),
+    ]);
+
+    return response.status(200).json({ posts, author });
   } catch (error) {
     return response.status(500).send(error.message);
   }
