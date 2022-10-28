401e19d099c00a8615365a15e77235c71a371ae4
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-10-28 10:51:06 -0300

    Add progress bar on route change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,17 @@ import { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { ToastContainer } from 'react-toastify';
 import { Session } from 'next-auth';
+import { useProgressBar } from '../hooks/use-progressbar';
 import 'react-toastify/dist/ReactToastify.css';
 import '../public/global.css';
+import 'nprogress/nprogress.css';
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps<{ session: Session }>) {
+  useProgressBar();
+
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
